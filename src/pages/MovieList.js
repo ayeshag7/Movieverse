9bf65a78c348877439f58765057dadd753c486e9
Ayesha <ayeshaiqbal2592@gmail.com>
2023-03-26 20:33:19 +0500

    Fixed (reading 'map') error

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -14,7 +14,7 @@ export const MovieList = ({ api_path, title }) => {
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap other:justify-evenly">
-          { movies.map((movie) => (
+          { movies && movies.map((movie) => (
             <Card key={movie.id} movie={movie}/>
           ))}
         </div>
